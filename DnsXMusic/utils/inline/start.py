74f5c7e3c70dcabf5eeb58de3d4dba69f2baebe8
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-30 17:18:34 +0530

    ʜᴀʀʀʏ

--- DnsXMusic/utils/inline/start.py
+++ DnsXMusic/utils/inline/start.py
@@ -12,7 +12,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from DnsXMusic import app
+from HarryXSarkar import app
 
 
 def start_pannel(_):
